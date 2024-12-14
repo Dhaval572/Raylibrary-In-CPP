9731a42a157d600b76986e17aef51f78a310c0de
Dhaval572 <dp9718776@gmail.com>
2024-12-14 12:38:29 +0530

    Create Raylib_importand_function_Part 2.cpp

---
+++ Raylib_importand_function_Part 2.cpp
@@ -0,0 +1,64 @@
+/*
+( Copy all the code in vs code to see perfect ) 
+Raylib Functions;
+
+    here is some explaination about important functions in raylib:
+
+        IsKeyDown():
+
+            This function checks for particular key is continuosly pressed or not.
+
+            Syntax:
+
+                IsKeyDown(Key_name)
+                {
+                    // do some task
+                }
+
+        Keys available for check is pressed or not:
+
+        Alphabet and Number Keys
+
+            KEY_A to KEY_Z : Keys for alphabetic characters (A to Z).
+
+            KEY_0 to KEY_9 : Number Keys( 0 to 9).
+
+        Special Characters and Modifier Keys
+
+        +-----------------------------------------------------------------------+
+        |    KEY_SPACE                 |             Spacebar.                  |
+        |    KEY_ENTER                 |             Enter (Return) key.        |
+        |    KEY_TAB                   |             Tab key.                   |
+        |    KEY_BACKSPACE             |             Backspace key.             |
+        |    KEY_SHIFT                 |             Shift key.                 |
+        |    KEY_CONTROL               |             Control (Ctrl) key.        |
+        |    KEY_ALT                   |             Alt key.                   |
+        |    KEY_CAPS_LOCK             |             Caps Lock key.             |
+        |    KEY_ESCAPE                |             Escape key.                |
+        |    KEY_F1 to KEY_F12         |             Function keys (F1 to F12). |
+        |    KEY_INSERT                |             Insert key.                |
+        |    KEY_DELETE                |             Delete key.                |
+        |    KEY_HOME                  |             Home key.                  |
+        |    KEY_END                   |             End key.                   |
+        |    KEY_PAGE_UP               |             Page Up key.               |
+        |    KEY_PAGE_DOWN             |             Page Down key.             |
+        |    KEY_ARROW_UP              |             Up arrow key.              |
+        |    KEY_ARROW_DOWN            |             Down arrow key.            |
+        |    KEY_ARROW_LEFT            |             Left arrow key.            |
+        |    KEY_ARROW_RIGHT           |             Right arrow key.           |
+        +-----------------------------------------------------------------------+
+
+        GetScreenWidth():
+
+            This function is provides maximum x position of graphics screen.
+
+            In short and simple we can say that this function returns the screen width.
+
+        GetScreenHeight():
+
+            This function is provides maximum y position of graphics screen.
+
+            In short and simple we can say that this function returns the screen height.
+
+    So, i hope you undertand every thing
+*/

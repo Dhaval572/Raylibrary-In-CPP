b132831d7337a2771590b396e507acb45c5acf01
Dhaval572 <dp9718776@gmail.com>
2024-12-19 12:12:14 +0530

    Create Display_image_in_raylib.cpp

---
+++ Display_image_in_raylib.cpp
@@ -0,0 +1,48 @@
+// Display the picture in the raylib
+#include <raylib.h>
+
+int main()
+{
+    InitWindow(800, 600, " ");
+
+    Texture2D backGround = LoadTexture("Images/background.png"); // To load an image
+    Vector2 imagePos = {0, 0};
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        DrawText("Hold Left or Right arrow key to see background image", 0, 0, 24, WHITE);
+
+        ClearBackground(BLACK);
+        if(IsKeyDown(KEY_LEFT))
+            DrawTexture(backGround, 0, 0, WHITE);
+
+        if(IsKeyDown(KEY_RIGHT))
+            DrawTextureV(backGround, imagePos, WHITE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
+/*
+To load the texture there are used:
+
+    LoadTexture("path/image_name.extention");
+
+To display in background:
+
+    1). DrawTexture(Texture_name, x_pos, y_Pos, color);
+
+        This is simple function.
+
+    2). DrawTexture(Texture_name, Texture_pos, color);
+
+        This function takes the Vector2 as position of image in graphics window.
+
+
+    So, this was function to display image in graphics window
+
+*/

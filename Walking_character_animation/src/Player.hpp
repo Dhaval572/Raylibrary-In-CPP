f2740b96309119f7d00c1701c1ba86ba137e6f6d
Dhaval572 <dp9718776@gmail.com>
2024-12-27 13:00:52 +0530

    Update Player.hpp

--- Walking_character_animation/src/Player.hpp
+++ Walking_character_animation/src/Player.hpp
@@ -1,17 +1,20 @@
+#include <raylib.h>
 #pragma once
 
 class Player
 {
 public:
+
 	Player(const char *walkRight1Path, const char *walkRight2Path,
 		   const char *walkLeft1Path, const char *walkLeft2Path,
 		   float x, float y, float speed);
 	~Player();
-	void Update();
-	void Draw();
+	void update();
+	void draw();
+	void handleMovement();
+	void updateAnimation();
 
 private:
-
 	Image resizeImage(const char *path, int width, int height);
 	Texture2D loadTextureFromResizedImage(const char *path, int width, int height);
 	Vector2 position;
@@ -21,6 +24,4 @@ private:
 	int frame;
 	float animationTime;
 	Texture2D walkRight1, walkRight2, walkLeft1, walkLeft2, background;
-	void handleMovement();
-	void updateAnimation();
 };

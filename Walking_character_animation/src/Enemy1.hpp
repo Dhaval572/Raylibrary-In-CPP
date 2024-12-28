1d82f4669ec1b4266e225c8e6312e4a4c7c80408
Dhaval572 <dp9718776@gmail.com>
2024-12-28 12:30:08 +0530

    Update Enemy1.hpp

--- Walking_character_animation/src/Enemy1.hpp
+++ Walking_character_animation/src/Enemy1.hpp
@@ -1,26 +1,31 @@
-#pragma once 
+#pragma once
 #include <raylib.h>
 
 class Enemy1
 {
 public:
-
 	Enemy1();
 	~Enemy1();
+	Vector2 enemyPos;
 	void draw();
 	void update(Player &player);
 	void handleMovement(Player &player);
 	void handleAnimation();
 
 private:
-
-	Image resizeImage(const char *path, int width, int height);
+	Image resizeTextureForWalk(const char *path, int width, int height);
+	Image resizeTextureForAttack(const char *path, int width, int height);
 	Texture2D loadTextureFromResizedImage(const char *path, int width, int height);
-	Vector2 position;
 	bool isMoving;
 	float speed;
+	float currentHealth;
+	float maxHealth;
+	Rectangle healthBar; 
 	int frame;
 	float animationTime;
 	Texture2D walk1, walk2, walk3, walk4, walk5, walk6;
-	void Attack();
+	Texture2D attack1, attack2, attack3, attack4;
+	void drawHealthBar();
+	void updateHealthBar();
+
 };

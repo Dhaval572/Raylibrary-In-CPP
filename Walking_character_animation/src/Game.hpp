481364e8281226e54c23d72aef1db5e820a14f5e
Dhaval572 <dp9718776@gmail.com>
2024-12-31 19:01:54 +0530

    Update Game.hpp

--- Walking_character_animation/src/Game.hpp
+++ Walking_character_animation/src/Game.hpp
@@ -1,12 +1,11 @@
 #pragma once
+#include <raylib.h>
 #include "Player.hpp"
 #include "Enemy1.hpp"
-#include <raylib.h>
 
 class Game
 {
 public:
-
 	Game();
 	~Game();
 	void Draw();
@@ -15,7 +14,8 @@ public:
 	void handleInput();
 
 private:
-
 	Player player; // Creating object
-	Enemy1 enemy1;	
+	Enemy1 enemy1; 
+	float damageDelayTimer;
+	const float damageDelayTime = 1.0f;
 };

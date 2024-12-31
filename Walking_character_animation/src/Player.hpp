8652c6e53d10800ca94a50d170a78f5443ed311b
Dhaval572 <dp9718776@gmail.com>
2024-12-31 19:01:03 +0530

    Update Player.hpp

--- Walking_character_animation/src/Player.hpp
+++ Walking_character_animation/src/Player.hpp
@@ -1,40 +1,41 @@
-#include <raylib.h>
 #pragma once
+#include <raylib.h>
 
 class Player
 {
 public:
-
-	Player();
-	~Player();
-	void update();
-	void draw();
-	void handleMovement();
-	void updateAnimation();
-	const Rectangle playerRect();
-	void talkDamage(float damage);
+    Player();
+    ~Player();
+    void update();
+    void draw();
+    void handleMovement();
+    void updateAnimation();
+    const Rectangle playerRect();
+    void talkDamage(float damage);
+    
+    // New method to handle taking damage
+    void takeDamage(float damage);
 
 private:
+    Image resizeImage(const char *path, int width, int height);
+    Texture2D loadTextureFromResizedImage(const char *path, int width, int height);
 
-	Image resizeImage(const char *path, int width, int height);
-	Texture2D loadTextureFromResizedImage(const char *path, int width, int height);
+    // Functions
+    void drawHealthBar();
+    void updateHealthBar();
 
-	// Functions
-	void drawHealthBar();
-	void updateHealthBar();
+    // Player properties
+    Vector2 position;
+    float speed;
+    float currentHealth;
+    float maxHealth;
+    Rectangle healthBar;
+    bool isMoving;
+    bool isMovingRight;
+    int frame;
+    float animationTime;
+    Color healthColor;
 
-	// Player properties
-	Vector2 position;
-	float speed;
-	float currentHealth;
-	float maxHealth;
-	Rectangle healthBar;
-	bool isMoving;
-	bool isMovingRight;
-	int frame;
-	float animationTime;
-	Color healthColor;
-
-	// Texture for player walking animation
-	Texture2D walkRight1, walkRight2, walkLeft1, walkLeft2, background;
+    // Texture for player walking animation
+    Texture2D walkRight1, walkRight2, walkLeft1, walkLeft2, background;
 };

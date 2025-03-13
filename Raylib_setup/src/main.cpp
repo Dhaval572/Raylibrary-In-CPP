5f583d3fdef2ede4396a0aa5cc8792ec4aeedcbc
Dhaval <dp9718776@gmail.com>
2025-03-13 12:33:44 +0530

    Create

---
+++ Raylib_setup/src/main.cpp
@@ -0,0 +1,20 @@
+#include <raylib.h>
+
+int main()
+{
+    InitWindow(600, 500, " ");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}

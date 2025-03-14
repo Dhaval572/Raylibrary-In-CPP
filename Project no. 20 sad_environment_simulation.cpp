d51a5bb2a80085a57420edd5b6d14b99fdb1bca8
Dhaval <dp9718776@gmail.com>
2025-03-14 15:50:00 +0530

    Improved tear animation

--- Project no. 20 sad_environment_simulation.cpp
+++ Project no. 20 sad_environment_simulation.cpp
@@ -12,8 +12,9 @@ constexpr int SCREEN_HEIGHT = 600;
 constexpr int TEAR_LIFETIME = 180;
 constexpr float GRAVITY = 0.1f;
 constexpr float SUN_SPEED = 0.05f;
-constexpr float CHARACTER_RADIUS = 15.0f;
-constexpr float TEXT_SPEED = 0.05f; // Speed of typing effect
+constexpr float CHARACTER_RADIUS = 20.0f;
+constexpr float TEXT_SPEED = 0.05f;
+constexpr float TEAR_MAX_DISTANCE = 40.5f;
 
 // Compile-time color definitions
 constexpr Color SKY_TOP{255, 140, 0, 255};
@@ -21,7 +22,7 @@ constexpr Color SKY_BOTTOM{30, 10, 50, 255};
 constexpr Color SUN_COLOR{250, 180, 50, 255};
 constexpr Color HILL_DARK{100, 50, 15, 255};
 constexpr Color HILL_LIGHT{139, 69, 19, 255};
-constexpr Color TEAR_COLOR{180, 180, 255, 230};
+constexpr Color TEAR_COLOR{230, 230, 255, 255};
 constexpr Color PERSON_COLOR{112, 128, 144, 255};
 
 // Typing effect variables
@@ -31,8 +32,18 @@ float startTime;
 struct Tear
 {
     float x, y;
+    float startY;
     float velocityY{0.5f};
+    float velocityX;
     int life{TEAR_LIFETIME};
+
+    Tear(float startX, float startY) : x(startX), y(startY), startY(startY)
+    {
+        if (startX < SCREEN_WIDTH / 2.0f)
+            velocityX = -0.8f; // Left eye tear falls diagonally left (/)
+        else
+            velocityX = 0.8f; // Right eye tear falls diagonally right (\)
+    }
 };
 
 // Update tears with gravity effect
@@ -40,13 +51,14 @@ void UpdateTears(vector<Tear> &tears)
 {
     for (auto &tear : tears)
     {
+        tear.x += tear.velocityX;
         tear.y += tear.velocityY;
         tear.velocityY += GRAVITY;
         --tear.life;
     }
 
     tears.erase(remove_if(tears.begin(), tears.end(), [](const Tear &t)
-                          { return t.life <= 0; }),
+                          { return t.life <= 0 || ((t.y - t.startY) > TEAR_MAX_DISTANCE); }),
                 tears.end());
 }
 
@@ -66,16 +78,23 @@ void DrawSkyGradient()
 
 void DrawCharacter(const Vector2 &pos)
 {
-    DrawCircle(pos.x, pos.y, CHARACTER_RADIUS, PERSON_COLOR);   // Head
-    DrawRectangle(pos.x - 8, pos.y + 10, 16, 25, PERSON_COLOR); // Body
+    // Head
+    DrawCircle(pos.x, pos.y, CHARACTER_RADIUS, PERSON_COLOR);
+
+    // Body
+    DrawRectangle(pos.x - 12, pos.y + CHARACTER_RADIUS, 24, 50, PERSON_COLOR);
+
+    // Eyes
+    DrawCircle(pos.x - 8, pos.y - 5, 4, BLACK); // Left eye
+    DrawCircle(pos.x + 8, pos.y - 5, 4, BLACK); // Right eye
 
     // Legs
-    DrawLine(pos.x - 6, pos.y + 30, pos.x - 12, pos.y + 42, PERSON_COLOR); // Left leg
-    DrawLine(pos.x + 6, pos.y + 30, pos.x + 12, pos.y + 42, PERSON_COLOR); // Right leg
+    DrawLine(pos.x - 8, pos.y + CHARACTER_RADIUS + 50, pos.x - 16, pos.y + CHARACTER_RADIUS + 80, PERSON_COLOR); // Left leg
+    DrawLine(pos.x + 8, pos.y + CHARACTER_RADIUS + 50, pos.x + 16, pos.y + CHARACTER_RADIUS + 80, PERSON_COLOR); // Right leg
 
     // Arms
-    DrawLine(pos.x - 8, pos.y + 15, pos.x - 20, pos.y + 25, PERSON_COLOR); // Left arm
-    DrawLine(pos.x + 8, pos.y + 15, pos.x + 20, pos.y + 25, PERSON_COLOR); // Right arm
+    DrawLine(pos.x - 12, pos.y + CHARACTER_RADIUS + 15, pos.x - 30, pos.y + CHARACTER_RADIUS + 40, PERSON_COLOR); // Left arm
+    DrawLine(pos.x + 12, pos.y + CHARACTER_RADIUS + 15, pos.x + 30, pos.y + CHARACTER_RADIUS + 40, PERSON_COLOR); // Right arm
 }
 
 // Display text with a typing effect in a speech format
@@ -89,34 +108,36 @@ void DrawTypingText(const Vector2 &charPos, int fontSize, Color color)
 
     // Center text above the character
     int textX = charPos.x - MeasureText(displayedText.c_str(), fontSize) / 2;
-    int textY = charPos.y - 50;
+    int textY = charPos.y - 70;
 
-    // Draw Speech
-    DrawLine(charPos.x, charPos.y - 20, charPos.x - 10, charPos.y - 35, color);
+    // Draw Speech connector line
+    DrawLine(charPos.x, charPos.y - 20, charPos.x - 10, charPos.y - 50, color);
     DrawText(displayedText.c_str(), textX, textY, fontSize, color);
 }
 
 int main()
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunset of a Broken Soul");
-    SetTargetFPS(60);
-
     vector<Tear> tears;
-    constexpr Vector2 CHARACTER_POS{SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT - 160};
+
+    // Adjusted position for larger character
+    constexpr Vector2 CHARACTER_POS{SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT - 180};
+
     float sunY = 100.0f;
     startTime = GetTime();
 
+    SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         if (sunY < 200)
             sunY += SUN_SPEED;
 
-        // Random tear generator from both eyes
-        if (GetRandomValue(0, 100) < 3)
-            tears.emplace_back(Tear{CHARACTER_POS.x - 5, CHARACTER_POS.y + 8});
-
-        if (GetRandomValue(0, 100) < 3)
-            tears.emplace_back(Tear{CHARACTER_POS.x + 5, CHARACTER_POS.y + 8});
+        // Generate tears from both eyes simultaneously
+        if (GetRandomValue(0, 100) < 50)
+        {
+            tears.emplace_back(Tear{CHARACTER_POS.x - 8, CHARACTER_POS.y - 5}); // Left eye tear
+            tears.emplace_back(Tear{CHARACTER_POS.x + 8, CHARACTER_POS.y - 5}); // Right eye tear
+        }
 
         UpdateTears(tears);
 
